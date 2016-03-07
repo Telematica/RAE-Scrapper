6e2cb2194c6b0ec3c2384ad29900230056107b3f
Héctor Cerón Figueroa <linuxeron@gmail.com>
2016-03-07 10:09:17 -0600

    Request updated

--- curl-test.sh
+++ curl-test.sh
@@ -2,5 +2,11 @@
 
 # Simple HTTP request to Buscon server (RAE)
 
-curl -v http://buscon.rae.es/drae/srv/search?val=api \
--d "TS014dfc77_id=3&TS014dfc77_cr=c563b25d20a81e2dca49edcbf9852373%3Alllk%3A2OB0CWtp%3A1150579394&TS014dfc77_76=0&TS014dfc77_md=1&TS014dfc77_rf=0&TS014dfc77_ct=0&TS014dfc77_pd=0"
+#deprecated
+#curl -v http://buscon.rae.es/drae/srv/search?val=api \
+#-d "TS014dfc77_id=3&TS014dfc77_cr=c563b25d20a81e2dca49edcbf9852373%3Alllk%3A2OB0CWtp%3A1150579394&TS014dfc77_76=0&TS014dfc77_md=1&TS014dfc77_rf=0&TS014dfc77_ct=0&TS014dfc77_pd=0"
+
+#upgraded for new endpoint http://dle.rae.es/
+curl -H "Upgrade-Insecure-Requests:1" \
+-H "User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36" \ 
+-X GET http://dle.rae.es/srv/fetch?w=manijero
